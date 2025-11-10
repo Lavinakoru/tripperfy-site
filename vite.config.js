e77418cabe009830d28e71f718da12e5c7bdcfc7
Lavina Korani <lavina.korani@gmail.com>
2025-11-11 00:05:49 +0530

    Initial commit

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,10 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
 export default defineConfig({
+  base: './', // 👈 This line ensures correct relative paths for Netlify
   plugins: [react()],
+  build: {
+    outDir: 'dist',
+  },
 })
